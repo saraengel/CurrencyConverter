ab64cd1f74a3af70f3278fd46906d1581a78db76
sara engel <suriengel10@gmail.com>
2022-04-04 19:15:48 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NPipe } from './n.pipe';
+import { ConvertComponent } from './components/convert/convert.component';
+import {DropdownModule} from 'primeng/dropdown';
+import {ButtonModule} from 'primeng/button';
+import { AboutComponent } from './components/about/about.component';
+import { MenuComponent } from './components/menu/menu.component';
+import {TabMenuModule} from 'primeng/tabmenu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NPipe,
+    ConvertComponent,
+    AboutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    DropdownModule,
+    FormsModule,
+    TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
